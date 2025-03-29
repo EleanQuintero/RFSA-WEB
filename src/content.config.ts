ee405348f14b6a87abd8af34cfb5e95d6fe83d30
EleanQuintero <elean.quintero.madrid@gmail.com>
2025-03-29 01:17:42 +0100

    Agregar sección de testimonios, implementar animaciones y mejorar estilos en varias páginas

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,17 +1,31 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
-export const collections = {
-	posts: defineCollection({
-		// Load Markdown files in the src/content/work directory.
-		loader: glob({ base: './src/content/posts', pattern: '**/*.md', }),
-		schema: z.object({
-			title: z.string(),
-			description: z.string(),
-			publishDate: z.coerce.date(),
-			tags: z.array(z.string()),
-			img: z.string(),
-			img_alt: z.string().optional(),
-		}),
+
+const posts = defineCollection({
+	loader: glob({ base: './src/content/posts', pattern: '**/*.md', }),
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		publishDate: z.coerce.date(),
+		tags: z.array(z.string()),
+		img: z.string(),
+		img_alt: z.string().optional(),
 	}),
+})
+
+const testimonials = defineCollection({
+	loader: glob({ base: './src/content/testimonials', pattern: '**/*.md', }),
+	schema: z.object({
+		name: z.string(),
+		sport: z.string(),
+		img: z.string(),
+		rating: z.string(),
+		testimonial: z.string(),
+		url: z.string()
+	}),
+})
+
+export const collections = {
+	posts, testimonials
 };
---
+++ src/content/testimonials/adrian-manzanares.md
@@ -0,0 +1,24 @@
+---
+id: adrian-manzanares
+name: "Adrian Manzanares"
+sport: "Powerlifting"
+img: "https://powerhispania.net/wp-content/uploads/2024/10/Adrian_Manzanares.jpg"
+rating: 5,
+testimonial: "Training here has completely transformed my performance in the pool. The facilities and coaching are world-class."
+url: "/testimonials/adrian-manzanares"
+---
+
+
+> "Frase del levantador"
+
+### Mi historia en Rehab for Strengh Atlhetes 
+
+Lorem ipsum dolum 
+
+### Por que RFSA 
+
+Elegí RFSA por esto y esto y esto 
+
+### Experiencia con RSFA 
+
+LOREM IPSUM DOLUM 
