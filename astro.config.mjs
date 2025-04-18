57eac5bdcd6e5d8f2abb42e2edc692e64bb42e41
EleanQuintero <elean.quintero.madrid@gmail.com>
2025-04-18 20:00:20 +0200

    updating deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,10 +12,23 @@ export default defineConfig({
     }),
     vite: {
         plugins: [tailwindcss()],
+        define: {
+            // Esto es necesario para que las variables de entorno estén disponibles en Vercel
+            "process.env.UPSTASH_REDIS_REST_URL": JSON.stringify(process.env.UPSTASH_REDIS_REST_URL),
+            "process.env.UPSTASH_REDIS_REST_TOKEN": JSON.stringify(process.env.UPSTASH_REDIS_REST_TOKEN),
+        },
     },
     srcDir: './src',
     outDir: './dist',
     base: '/',
     integrations: [react()],
-    session: true,
+    session: {
+        // Configuración para usar Redis como almacenamiento de sesiones
+        driver: 'redis',
+        options: {
+            // Estas variables se configurarán automáticamente si usas la integración de Upstash con Vercel
+            url: process.env.UPSTASH_REDIS_REST_URL,
+            token: process.env.UPSTASH_REDIS_REST_TOKEN,
+        },
+    },
 });
