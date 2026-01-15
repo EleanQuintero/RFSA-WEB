b136afd4668824a64743767682bffdffa68311d6
EleanQuintero <elean.quintero.madrid@gmail.com>
2026-01-15 12:01:16 +0100

    feat: image preview changed

--- src/components/react/ReactFilters.tsx
+++ src/components/react/ReactFilters.tsx
@@ -1,22 +1,23 @@
-import React, { useState } from 'react';
-import type { CollectionEntry } from 'astro:content';
+import React, { useState } from "react";
+import type { CollectionEntry } from "astro:content";
 
 interface ReactFiltersProps {
-  posts: CollectionEntry<'posts'>[];
+  posts: CollectionEntry<"posts">[];
   categories: string[];
 }
 
 const ReactFilters: React.FC<ReactFiltersProps> = ({ posts, categories }) => {
-  const [filteredPosts, setFilteredPosts] = useState<CollectionEntry<'posts'>[]>(posts);
-  const [activeCategory, setActiveCategory] = useState<string>('all');
+  const [filteredPosts, setFilteredPosts] =
+    useState<CollectionEntry<"posts">[]>(posts);
+  const [activeCategory, setActiveCategory] = useState<string>("all");
 
   const handleCategoryChange = (category: string) => {
     setActiveCategory(category);
-    
-    if (category === 'all') {
+
+    if (category === "all") {
       setFilteredPosts(posts);
     } else {
-      const filtered = posts.filter(post => post.data.tags[0] === category);
+      const filtered = posts.filter((post) => post.data.tags[0] === category);
       setFilteredPosts(filtered);
     }
   };
@@ -26,9 +27,11 @@ const ReactFilters: React.FC<ReactFiltersProps> = ({ posts, categories }) => {
       <div className="filter-buttons flex gap-4 flex-wrap justify-center my-8">
         <button
           className={`px-6 py-2 rounded-md transition-colors ${
-            activeCategory === 'all' ? ' bg-[var(--gray-900)] text-[var(--gray-100)]' : 'bg-gray-200 hover:bg-gray-300'
+            activeCategory === "all"
+              ? " bg-[var(--gray-900)] text-[var(--gray-100)]"
+              : "bg-gray-200 hover:bg-gray-300"
           }`}
-          onClick={() => handleCategoryChange('all')}
+          onClick={() => handleCategoryChange("all")}
         >
           Todos
         </button>
@@ -36,7 +39,9 @@ const ReactFilters: React.FC<ReactFiltersProps> = ({ posts, categories }) => {
           <button
             key={category}
             className={`px-6 py-2 rounded-md transition-colors ${
-              activeCategory === category ? 'bg-[var(--gray-900)] text-[var(--gray-100)]' : 'bg-gray-200 hover:bg-gray-300'
+              activeCategory === category
+                ? "bg-[var(--gray-900)] text-[var(--gray-100)]"
+                : "bg-gray-200 hover:bg-gray-300"
             }`}
             onClick={() => handleCategoryChange(category)}
           >
@@ -47,16 +52,16 @@ const ReactFilters: React.FC<ReactFiltersProps> = ({ posts, categories }) => {
 
       <div className="flex flex-col items-center w-full justify-center lg:grid lg:grid-cols-3 gap-10 lg:max-w-6xl mx-auto">
         {filteredPosts.map((post) => (
-          <a 
-            key={post.id} 
+          <a
+            key={post.id}
             href={`/post/${post.id}/`}
             className="post-item block bg-[var(--gray-900)] rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow max-w-sm w-full"
           >
-            {post.data.img && (
+            {post.data.img_cover && (
               <div className="relative w-full">
-                <img 
-                  src={post.data.img} 
-                  alt={post.data.img_alt || post.data.title} 
+                <img
+                  src={post.data.img_cover}
+                  alt={post.data.img_alt || post.data.title}
                   className="w-full h-48 object-cover"
                   data-astro-transition-name={`img-${post.data.title}`}
                 />
@@ -67,18 +72,17 @@ const ReactFilters: React.FC<ReactFiltersProps> = ({ posts, categories }) => {
             )}
             <div className="p-6">
               <h3 className="text-2xl font-bold mb-3">{post.data.title}</h3>
-              <p className="text-[var(--gray-100)] mb-4">{post.data.description}</p>
+              <p className="text-[var(--gray-100)] mb-4">
+                {post.data.description}
+              </p>
               <div className="flex justify-between items-center">
-                {
-                  /*<p className="text-gray-500 text-sm">
+                {/*<p className="text-gray-500 text-sm">
                   {new Date(post.data.publishDate).toLocaleDateString('es-ES', {
                     year: 'numeric',
                     month: 'long',
                     day: 'numeric'
                   })}
-                </p>*/
-                }
-                
+                </p>*/}
               </div>
             </div>
           </a>
