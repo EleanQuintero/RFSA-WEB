28f068e9b50dab63f4ef81a3d9af1239a6d0e165
EleanQuintero <elean.quintero.madrid@gmail.com>
2026-01-15 12:00:33 +0100

    feat: content config changed

--- src/content.config.ts
+++ src/content.config.ts
@@ -9,7 +9,7 @@ const posts = defineCollection({
 		description: z.string(),
 		publishDate: z.coerce.date(),
 		tags: z.array(z.string()),
-		img: z.string(),
+		img_cover: z.string(),
 		img_alt: z.string().optional(),
 	}),
 })
